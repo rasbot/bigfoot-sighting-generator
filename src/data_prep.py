6be45727c01aafaad064966d472257a4c808a05d
RasNath <nathan.f.rasmussen@gmail.com>
2020-09-14 14:18:03 -0700

    add method to generate states data for randomizing sighting info

--- src/data_prep.py
+++ src/data_prep.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import json
+import numpy as np
 
 
 def import_data(filepath):
@@ -37,3 +38,39 @@ def create_df(reports):
     df = df.loc[features]
     reports_df = df.transpose()
     return reports_df
+
+
+def get_state_data(df, folder):
+    """
+    From the main dataframe, gets a list of unique states that have had sightings
+    and creates a dictionary that has counties, nearest towns, and nearest roads
+    associated with sightings from those states. Writes the dictionary to a json
+    file called 'states.json'.
+
+        Parameters:
+                df (dataframe): The main sightings dataframe
+                folder (str): The folder where the json file is stored.
+
+        Returns:
+                None.
+    """
+    unique_states = np.unique(np.array(df['STATE'].dropna()))
+    idx = np.argwhere(unique_states == '(International)')
+    unique_states = np.delete(unique_states, idx)
+
+    counties_d, nearest_town_d, nearest_road_d = dict(), dict(), dict()
+    for i in range(len(unique_states)):
+        counties_d[unique_states[i]] = np.unique(
+            df[df.STATE == unique_states[i]]['COUNTY'].dropna()).tolist()
+        nearest_town_d[unique_states[i]] = np.unique(
+            df[df.STATE == unique_states[i]]['NEAREST TOWN'].dropna()).tolist()
+        nearest_road_d[unique_states[i]] = np.unique(
+            df[df.STATE == unique_states[i]]['NEAREST ROAD'].dropna()).tolist()
+    d = dict()
+    for i in unique_states:
+        d[i] = {"Counties": counties_d[i],
+                "Nearest Town": nearest_town_d[i],
+                "Nearest Road": nearest_road_d[i]}
+
+    with open(f"{folder}/states.json", "w") as f:
+        json.dump(d, f, indent=4, sort_keys=False)
