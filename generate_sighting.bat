1d000bf70f391b64bc7fd7395a6aa9cebef30797
RasNath <nathan.f.rasmussen@gmail.com>
2020-09-15 17:28:18 -0700

    add script to batch file

--- generate_sighting.bat
+++ generate_sighting.bat
@@ -3,3 +3,5 @@
 python sample.py --save_dir="data/observed/save" -n=250 -s=True -t="observed"
 python sample.py --save_dir="data/environment/save" -n=30 -s=True -t="environment"
 python sample.py --save_dir="data/time_conditions/save" -n=20 -s=True -t="time_conditions"
+
+python generate_sighting.py
