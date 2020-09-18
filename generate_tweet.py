04bd21a4e81a886c25f33d7122bb0f30dd5f5f1a
RasNath <nathan.f.rasmussen@gmail.com>
2020-09-18 15:14:14 -0700

    add function to remove non-ascii characters, improved split_tweet function

--- generate_tweet.py
+++ generate_tweet.py
@@ -1,14 +1,21 @@
 import json
 import time
 import os
+import string
 import tweepy
 
-def split_tweet(observation, sighting_info, max_length=225):
+def remove_non_ascii(s):
+    s = s.replace('’', '\'')
+    printable = list(string.printable)
+    return "".join(filter(lambda x: x in printable, s))
+
+def split_tweet(observation, sighting_info, only_obs=False, numbered=True, max_length=225):
     o = ""
+    observation = remove_non_ascii(observation)
     obs_split = observation.split(". ")
     for i in range(1,len(obs_split)):
         o += obs_split[i].capitalize() + ". "
-    observation = obs_split[0] + o
+    observation = obs_split[0] + ". " + o
     obs_split = observation.split(' ')
     obs_list, sighting_list, tweet_list, numbered_tweets = [], [], [], []
     s = ""
@@ -21,25 +28,32 @@ def split_tweet(observation, sighting_info, max_length=225):
             _ = val + " "
             s = _
     obs_list.append(s)
-    sighting_list = []
-    s = ""
-    for i in sighting_info:
-        if len(s) < max_length:
-            _ = i + "\n"
-            s += _
-        if len(s) > max_length:
-            s = s.replace(_, "")
-            sighting_list.append(s)
-            s = "" + i
-    sighting_list.append(s)
-    sighting_list[0] = sighting_list[0][:-1]
-    for i in sighting_list:
-        tweet_list.append(i)
+    if only_obs:
+        obs_list[0] = "".join(obs_list[0].split(": ")[1:])
+    else:
+        sighting_list = []
+        s = ""
+        sighting_info = remove_non_ascii(sighting_info)
+        for i in sighting_info:
+            if len(s) < max_length:
+                _ = i + "\n"
+                s += _
+            if len(s) > max_length:
+                s = s.replace(_, "")
+                sighting_list.append(s)
+                s = "" + i
+        sighting_list.append(s)
+        sighting_list[0] = sighting_list[0][:-1]
+        for i in sighting_list:
+            tweet_list.append(i)
     for j in obs_list:
         tweet_list.append(j)
-    for i in range(len(tweet_list)):
-        numbered_tweets.append(tweet_list[i] + " {}/{}".format(i+1, len(tweet_list)))
-    return numbered_tweets
+    if numbered:
+        for i in range(len(tweet_list)):
+            numbered_tweets.append(tweet_list[i] + " ({}/{})".format(i+1, len(tweet_list)))
+        return numbered_tweets
+    else:
+        return tweet_list
 
 with open("data/auth.json") as f:
     keys = json.load(f)
@@ -69,7 +83,7 @@ sighting_info = ["SEASON: {season}".format(season=latest_sighting["SEASON: "]),
 
 observation = """OBSERVATION: {obs}""".format(obs=latest_sighting["OBSERVATION: "])
 
-split_tweets = split_tweet(observation, sighting_info)
+split_tweets = split_tweet(observation, sighting_info, only_obs=True)
 
 with open("my_tweet.txt", "w") as f:
     f.write(split_tweets[0])
