87703dee4133cacf3bc871e239d6b8ce545aedd7
RasNath <nathan.f.rasmussen@gmail.com>
2020-09-21 21:43:51 -0700

    finish tweet script

--- generate_tweet.py
+++ generate_tweet.py
@@ -1,17 +1,23 @@
 import json
 import time
 import os
+import re
 import string
 import tweepy
+import numpy as np
 
 def remove_non_ascii(s):
+    rep = ['"', '(', ')']
     s = s.replace('’', '\'')
+    for i in rep:
+        s = s.replace(i, '')
     printable = list(string.printable)
     return "".join(filter(lambda x: x in printable, s))
 
-def split_tweet(observation, sighting_info, only_obs=False, numbered=True, max_length=225):
+def split_tweet(observation, sighting_info, only_obs=False, numbered=True, max_length=235):
     o = ""
     observation = remove_non_ascii(observation)
+    observation = re.sub(r'(?<=[.,])(?=[^\s^\.])', r' ', observation)
     obs_split = observation.split(". ")
     for i in range(1,len(obs_split)):
         o += obs_split[i].capitalize() + ". "
@@ -55,45 +61,54 @@ def split_tweet(observation, sighting_info, only_obs=False, numbered=True, max_l
     else:
         return tweet_list
 
-with open("data/auth.json") as f:
-    keys = json.load(f)
-f.close()
+def tweet():
+    with open("data/auth.json") as f:
+        keys = json.load(f)
+    f.close()
 
-# authentication of consumer key and secret 
-auth = tweepy.OAuthHandler(keys["consumer_key"], keys["consumer_secret"]) 
-  
-# authentication of access token and secret 
-auth.set_access_token(keys["access_token"], keys["access_token_secret"]) 
-api = tweepy.API(auth) 
+    # authentication of consumer key and secret 
+    auth = tweepy.OAuthHandler(keys["consumer_key"], keys["consumer_secret"]) 
+    
+    # authentication of access token and secret 
+    auth.set_access_token(keys["access_token"], keys["access_token_secret"]) 
+    api = tweepy.API(auth) 
 
-with open("generated_sightings.json") as f:
-    data = json.load(f)
+    with open("generated_sightings.json") as f:
+        data = json.load(f)
 
-# key of the newest entry
-latest_key = sorted(data.keys())[-1]
+    # key of the random entry
+    random_key = np.random.choice(list(data.keys()))
 
-latest_sighting = data[latest_key]
+    random_sighting = data[random_key]
 
-sighting_info = ["SEASON: {season}".format(season=latest_sighting["SEASON: "]),
-"STATE: {state}".format(state=latest_sighting["STATE: "]),
-"NEAREST TOWN: {nearest_town}".format(nearest_town=latest_sighting["NEAREST TOWN: "]),
-"NEAREST ROAD: {nearest_road}".format(nearest_road=latest_sighting["NEAREST ROAD: "]),
-"ENVIRONMENT: {env}".format(env=latest_sighting["ENVIRONMENT: "]),
-"TIME AND CONDITIONS: {tc}".format(tc=latest_sighting["TIME AND CONDITIONS: "])]
+    sighting_info = ["SEASON: {season}".format(season=random_sighting["SEASON: "]),
+    "STATE: {state}".format(state=random_sighting["STATE: "]),
+    "NEAREST TOWN: {nearest_town}".format(nearest_town=random_sighting["NEAREST TOWN: "]),
+    "NEAREST ROAD: {nearest_road}".format(nearest_road=random_sighting["NEAREST ROAD: "]),
+    "ENVIRONMENT: {env}".format(env=random_sighting["ENVIRONMENT: "]),
+    "TIME AND CONDITIONS: {tc}".format(tc=random_sighting["TIME AND CONDITIONS: "])]
 
-observation = """OBSERVATION: {obs}""".format(obs=latest_sighting["OBSERVATION: "])
+    observation = """OBSERVATION: {obs}""".format(obs=random_sighting["OBSERVATION: "])
 
-split_tweets = split_tweet(observation, sighting_info, only_obs=True)
+    # delete sighting from JSON file
+    del data[random_key]
+    with open("generated_sightings.json", "w") as f:
+        data = json.dump(data, f, indent=4, sort_keys=False)
 
-with open("my_tweet.txt", "w") as f:
-    f.write(split_tweets[0])
-with open('my_tweet.txt','r') as f:
-    tweet = api.update_status(f.read())
-for i in range(1,len(split_tweets)):
+    split_tweets = split_tweet(observation, sighting_info, only_obs=True)
+
     with open("my_tweet.txt", "w") as f:
-        f.write(split_tweets[i])
+        f.write(split_tweets[0])
     with open('my_tweet.txt','r') as f:
-        tweet = api.update_status(f.read(), in_reply_to_status_id = tweet.id_str)
-    time.sleep(5)
+        tweet = api.update_status(f.read())
+    for i in range(1,len(split_tweets)):
+        with open("my_tweet.txt", "w") as f:
+            f.write(split_tweets[i])
+        with open('my_tweet.txt','r') as f:
+            tweet = api.update_status(f.read(), in_reply_to_status_id = tweet.id_str)
+        time.sleep(5)
 
-exit()
+if __name__ == "__main__":
+    while True:
+        tweet()
+        time.sleep(14400)
