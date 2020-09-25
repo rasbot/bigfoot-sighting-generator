b6fbe79572dfd06657cc5294ec692c927e26f546
RasNath <nathan.f.rasmussen@gmail.com>
2020-09-25 16:22:14 -0700

    update script

--- generate_tweet.py
+++ generate_tweet.py
@@ -14,7 +14,7 @@ def remove_non_ascii(s):
     printable = list(string.printable)
     return "".join(filter(lambda x: x in printable, s))
 
-def split_tweet(observation, sighting_info, only_obs=False, numbered=True, max_length=235):
+def split_tweet(observation, sighting_info=None, only_obs=False, numbered=True, max_length=235):
     o = ""
     observation = remove_non_ascii(observation)
     observation = re.sub(r'(?<=[.,])(?=[^\s^\.])', r' ', observation)
@@ -73,7 +73,7 @@ def tweet():
     auth.set_access_token(keys["access_token"], keys["access_token_secret"]) 
     api = tweepy.API(auth) 
 
-    with open("generated_sightings.json") as f:
+    with open("gpt2_sightings.json") as f:
         data = json.load(f)
 
     # key of the random entry
@@ -81,18 +81,18 @@ def tweet():
 
     random_sighting = data[random_key]
 
-    sighting_info = ["SEASON: {season}".format(season=random_sighting["SEASON: "]),
-    "STATE: {state}".format(state=random_sighting["STATE: "]),
-    "NEAREST TOWN: {nearest_town}".format(nearest_town=random_sighting["NEAREST TOWN: "]),
-    "NEAREST ROAD: {nearest_road}".format(nearest_road=random_sighting["NEAREST ROAD: "]),
-    "ENVIRONMENT: {env}".format(env=random_sighting["ENVIRONMENT: "]),
-    "TIME AND CONDITIONS: {tc}".format(tc=random_sighting["TIME AND CONDITIONS: "])]
-
-    observation = """OBSERVATION: {obs}""".format(obs=random_sighting["OBSERVATION: "])
+    # sighting_info = ["SEASON: {season}".format(season=random_sighting["SEASON: "]),
+    # "STATE: {state}".format(state=random_sighting["STATE: "]),
+    # "NEAREST TOWN: {nearest_town}".format(nearest_town=random_sighting["NEAREST TOWN: "]),
+    # "NEAREST ROAD: {nearest_road}".format(nearest_road=random_sighting["NEAREST ROAD: "]),
+    # "ENVIRONMENT: {env}".format(env=random_sighting["ENVIRONMENT: "]),
+    # "TIME AND CONDITIONS: {tc}".format(tc=random_sighting["TIME AND CONDITIONS: "])]
+    sighting_info = None
+    observation = """OBSERVATION: {obs}""".format(obs=random_sighting)
 
     # delete sighting from JSON file
     del data[random_key]
-    with open("generated_sightings.json", "w") as f:
+    with open("gpt2_sightings.json", "w") as f:
         data = json.dump(data, f, indent=4, sort_keys=False)
 
     split_tweets = split_tweet(observation, sighting_info, only_obs=True)
